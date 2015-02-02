41e0c128475b6de56daf72c4fbd302458e8cbb44
Treasure Porth <treasure.rose@gmail.com>
2015-02-01 20:03:00 -0800

    game routes to mockup page

--- client/home.js
+++ client/home.js
@@ -1,10 +1,14 @@
+function otherId(game){
+	return game.currentTurn[game.currentTurn[0] === Meteor.userId() ? 1 : 0]; 
+}
+
 Template.userList.helpers({
 	users : function(){
 		var myid = Meteor.userId(),
 		cantPlay = [myid];
 
 	Games.find({ inProgress: true }).forEach(function (game) {
-		cantPlay.push(game.currentTurn[game.currentTurn[0] === myid ? 1 : 0]);
+		cantPlay.push(otherId(game));
 	});
 
 	return Meteor.users.find({_id: {$not: { $in: cantPlay}}}); 
@@ -18,9 +22,24 @@ Template.userList.helpers({
 
 Template.userItem.events({
 'click button' : function(event, template){
-		Meteor.call('createGame', template.data._id);
+	Meteor.call('createGame', template.data._id);
 	}
 });
+
+
+
+
+Template.gameList.helpers({
+	games: function(){
+		return Games.find({ inProgress: true}).map(function (game){
+			game.opponent = Meteor.users.findOne(otherId(game)).username;
+			game.started = moment(game.started).fromNow();
+			return game; 
+		});
+	}
+});
+
+
 
 Accounts.ui.config({
 	passwordSignupFields: "USERNAME_ONLY"
